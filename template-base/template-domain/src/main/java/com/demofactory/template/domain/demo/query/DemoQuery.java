94e380fca3350c6e0e057f5bb859e8ecac5aef5c
Mad98 <w_yyy@outlook.com>
2020-08-15 19:20:48 +0800

    增加不同配置文件，通过--spring.profiles.active= 来启动不同的环境

---
+++ template-base/template-domain/src/main/java/com/demofactory/template/domain/demo/query/DemoQuery.java
@@ -0,0 +1,10 @@
+package com.demofactory.template.domain.demo.query;
+
+import com.demofactory.template.domain.demo.Demo;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class DemoQuery extends Demo {
+}
